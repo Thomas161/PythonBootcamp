275a8d7e46d9bd18c7d5e9cf6ee1d57a94b09dbe
Tom <33819058+Thomas161@users.noreply.github.com>
2023-04-06 13:19:02 +1000

    Update addingDotsToString.py

--- addingDotsToString.py
+++ addingDotsToString.py
@@ -1,16 +1,14 @@
-# In progress
+# add and remove dots between characters
 
 def add_dots(s):
-    res = ""
-    for r in range(len(s)):
-        res += s[r]+"."
-        if res[len(res)-1] == ".":
-            print(res[len(res)-1])
-            res[len(res)-1].replace(".","")
-            # # res[:-1]
-            # print(res)
-            # res += s[r]+""
-            # print(len(res[:-1]))
-    return res
+    print(".".join(s))
+return ".".join(s)
     
 add_dots("Name")
+
+def remove_dots(s):
+    res = s.replace(".","")
+    print(res)
+    return res
+    
+remove_dots("m.a.m")
